abd89a4377ab1d29f61956e6647fd92b5b94030a
Utku Çakar <cakarutku1@hotmail.com>
2024-11-12 10:40:18 +0300

    Added a new class for group

---
+++ Models/Classes/GroupClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTricariOtomasyon.Models.Classes
+{
+    public class GroupClass
+    {
+        public string City { get; set; }
+        public int NumberOfCities { get; set; }
+    }
+}
